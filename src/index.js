2c5f5cf7d5fdce023b06566b3f068d88ea14931b
Abdul Rehman <ar618211@gmail.com>
2023-08-26 08:30:12 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import express from "express";
+import bodyParser from "body-parser";
 import "dotenv/config";
+
+const routes = [
+  '/api/echo',
+  '/docs',
+]
+
+const custom404 = (req, res, next) => {
+  if (!routes.includes(req.url)) {
+    res.status(404).sendFile('custom404.html', { root: './public' })
+  } else {
+    next()
+  }
+}
 
 function main() {
   const app = express()
-  const port = 3000
-  
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
+  const port = process.env.PORT || 5000
+
+  // middlewares
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw());
+  app.use(custom404)
+
+  // routes
+  app.get('/docs', (req, res) => {
+    res
+      .status(200)
+      .sendFile('docs.html', { root: './public' })
+  })
+  app.post('/api/echo', (req, res) => {
+    const reqBody = req.body
+    res
+      .status(200)  
+      .json(reqBody)
+    res.end()
   })
   
+  // start server
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server up on ${port}`)
   })
 }
 
